7bc59ece5ea839502917a405f799b306f3a1ae83
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-11-15 21:28:05 +0000

    Added the date to the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -13,6 +14,7 @@ export default function Weather() {
       city: response.data.name,
       day: `Saturday`,
       time: `11:11`,
+      date: new Date(response.data.dt*1000),
       humidity: Math.round(response.data.main.humidity),
       description: response.data.weather[0].description,
     });
@@ -37,6 +39,7 @@ export default function Weather() {
                 <li>{weatherData.city}</li>
                 <li>{weatherData.day}</li>
                 <li>{weatherData.time}</li>
+                <FormattedDate date={weatherData.date} />
               </ul>
             </div>
 
@@ -53,8 +56,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "e4c991b27b566dc4b5b311b6f8d9ac5c";
-    let city = "Brighton";
-    let apiUrl = ` http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    let apiUrl = ` http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
