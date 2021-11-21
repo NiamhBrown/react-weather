190cc6cf6b28514fb349233576f4d59ec34743c7
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-11-21 12:34:38 +0000

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,27 +4,24 @@ import FormattedDate from "./FormattedDate";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      
+      <p className="main-temp">{props.data.temperature}°C</p>
+      <div className="row">
+        <div className="col-6 info-left">
+          <ul>
+            <li>{props.data.city}</li>
 
-          <p className="main-temp">{props.data.temperature}°C</p>
-          <div className="row">
-            <div className="col-6 info-left">
-              <ul>
-                <li>{props.data.city}</li>
-
-                <FormattedDate date={props.data.date} />
-              </ul>
-            </div>
-
-            <div className="col-6 info-right">
-              <ul>
-                <li className="text-capitalize">{props.data.description}</li>
-                <li>Humidity: {props.data.humidity}%</li>
-                <li>Wind: {props.data.wind} mph</li>
-              </ul>
-            </div>
-          </div>
+            <FormattedDate date={props.data.date} />
+          </ul>
         </div>
-      
+
+        <div className="col-6 info-right">
+          <ul>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
