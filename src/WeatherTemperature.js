7bf7eafaaad0d7455372609755426ed891819978
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-11-29 20:46:56 +0000

    added unit conversion links

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 
-export default function WeatherTemperature(props){
-    const [unit, setUnit] = useState ("celsius")
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
 
-    function showFahrenheit(event){
-        event.preventDefault();
-        setUnit("fahrenheit");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
 
-    }
-    if (unit === `celsius`){
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === `celsius`) {
     return (
       <div className="WeatherTemperature">
         <p className="main-temp">
           {props.celsius}
           <sup className="units">
-            °C | <a href="/" onClick={showFahrenheit}>°F</a>
+            °C |{" "}
+            <a href="/" onClick={showFahrenheit}>
+              °F
+            </a>
           </sup>{" "}
         </p>
       </div>
-      );}
-    else {
-    return(
-        "F"
     );
-    }
+  } else {
+      let fahrenheit =Math.round((props.celsius *9/5) + 32)
+    return (
+      <div className="WeatherTemperature">
+        <p className="main-temp">
+          {fahrenheit}
+          <sup className="units">
+            <a href="/" onClick={showCelsius}>
+              °C{" "}
+            </a>
+            | °F
+          </sup>{" "}
+        </p>
+      </div>
+    );
+  }
 }
